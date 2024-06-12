92271889cdad1faa1156ab2359760c84b7b0a50d
Farha Islam <farha.islam@students.codeinstitute.net>
2024-06-12 11:09:38 +0000

    Add javascript functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,121 +1,151 @@
-/**
- * Read DOM elements
- */
+//Add questions and answers
 
-const Heading = document.querySelector(".title-container");
-const StartButton = document.querySelector(".start-btn button");
-const InfoBox = document.querySelector(".info-box");
-const ExitButton = InfoBox.querySelector(".buttons .quit-btn");
-const ContinueButton = InfoBox.querySelector(".buttons .restart-btn");
-const QuizBox = document.querySelector(".quiz-box"); 
-const TimeCount = document.querySelector(".timer .timer-sec");
-const TimeLine = document.querySelector("header .time-line");
-const Question = document.getElementById("question");
-const AnswerOptions = document.querySelector(".answer-options");
-const ResultBox = document.querySelector(".result-box");
-const RestartButton = ResultBox.querySelector(".buttons .restart-btn");
-const QuitBtn = ResultBox.querySelector(".buttons .quit-btn");
-const PageFooter = document.querySelector(".page-footer");
-
-/**
- * Variables for Quiz States
- */
-
-let time = 12; // Initial time to give answer
-let timerInterval; // Interval for the timer countdown
-let timerLineAnimtion; // Interval for the timer line animation
-let currentQuestionIndex = 0; // Index of the current question
-let correctScore = 0; // Number of correct answers
-let incorrectScore = 0; // Number of incorrect answers
-
-// START BUTTON
-
-// Event handler for the start button click
-StartButton.onclick = () => {
-    try {
-      questions = shuffle(questions ); // Shuffle the questions
-      Heading.classList.add("hide"); // Hide the heading element
-      PageFooter.classList.add("hide");
-      InfoBox .classList.add("activeInfo"); // Show the information box
-    } catch (error) {
-      alert("Oops! Gremlins invaded the Mushroom Quiz machinery. Try again later!");
-    }
-  };
-
-// Event handler for the continue button click
-ContinueButton.onclick = () => {
-    InfoBox.classList.remove("activeInfo"); // Hide Info box
-    QuizBox.classList.add("activeQuiz"); // Show Quiz box
-    showQuestions(); //Dislpay Questions
-    startTimer(); // Start Timer countdown
-    startTimerLine(); // Start Timer Line
-  };
+let questions = [{
+    numb: 1,
+    question: "What is the longest river in the world?",
+    answer: "Nile",
+    options: ["Nile",
+      "Danube",
+      "Amazon River",
+      "Amur"
+    ]
+  },
 
 
+  {
+    numb: 2,
+    question: "What country has the most natural lakes?",
+    answer: "Canada",
+    options: ["France",
+      "Poland",
+      "Canada",
+      "Japan"
+    ]
+  },
 
-  // Array passing the questions, answer options and setting the correct answer
-let questions = [
-    {
-      question: "What is the longest river in the world?",
-      answers: [
-        { text: "Nile", correct: true },
-        { text: "Danube", correct: false },
-        { text: "Amazon River", correct: false },
-        { text: "Amur", correct: false },
-      ],
-    },
-    {
-      question: "What country has the most natural lakes??",
-      answers: [
-        { text: "France", correct: false },
-        { text: "Poland", correct: false },
-        { text: "Canada", correct: true },
-        { text: "Japan", correct: false },
-      ],
-    },
-    {
-      question: "What is the capital city of Australia?",
-      answers: [
-        { text: "Sydney", correct: false },
-        { text: "Canberra", correct: true },
-        { text: "Perth", correct: false },
-        { text: "Melbourne", correct: false },
-      ],
-    },
-    {
-      question: "Who discovered penicillin?",
-      answers: [
-        { text: "Alexander Fleming", correct: true },
-        { text: "Marie Curie", correct: false },
-        { text: "Isaac Newton", correct: false },
-        { text: "Albert Einstein", correct: false },
-      ],
-    },
-    {
-      question: "Mount Everest is located in which mountain range?",
-      answers: [
-        { text: "Pyrenees", correct: true },
-        { text: "Karakoram", correct: false },
-        { text: "Alps", correct: false },
-        { text: "Himalayas", correct: true },
-      ],
-    },
-    {
-      question: "What is the largest country in Africa by area?",
-      answers: [
-        { text: "Ethiopia", correct: false },
-        { text: "Algeria", correct: true },
-        { text: "Egypt", correct: false },
-        { text: "Cameroon", correct: false },
-      ],
-    },
-    {
-      question: "Which country is known as the Land of the Rising Sun?",
-      answers: [
-        { text: "Thailand", correct: false },
-        { text: "China", correct: false },
-        { text: "Korea", correct: false },
-        { text: "Japan", correct: true },
-      ],
-    },
-  ];
+  {
+    numb: 3,
+    question: "What is the capital city of Australia?",
+    answer: "Canberra",
+    options: ["Sydney",
+      "Canberra",
+      "Perth",
+      "Melbourne"
+    ]
+  },
+
+  {
+    numb: 4,
+    question: "Who discovered penicillin?",
+    answer: "Alexander Fleming",
+    options: ["Alexander Fleming",
+      "Marie Curie",
+      "Isaac Newton",
+      "Albert Einstein"
+    ]
+  },
+
+  {
+    numb: 5,
+    question: "Mount Everest is located in which mountain range?",
+    answer: "Himalayas",
+    options: ["Pyrenees",
+      "Karakoram",
+      "Alps",
+      "Himalayas"
+    ]
+  },
+
+
+  {
+    numb: 6,
+    question: "What is the largest country in Africa by area?",
+    answer: "Algeria",
+    options: ["Ethiopia",
+      "Algeria",
+      "Egypt",
+      "Cameroon"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Which country is known as the Land of the Rising Sun?",
+    answer: "Japan",
+    options: ["Thailand",
+      "China",
+      "Korea",
+      "Japan"
+    ]
+  },
+
+]
+
+//Reading DOM elements
+
+const startBtn = document.querySelector(".start-btn button");
+const infoBox = document.querySelector(".info-box");
+const exitBtn = infoBox.querySelector(".buttons .quit-btn");
+const continueBtn = infoBox.querySelector(".buttons .restart-btn");
+const quizBox = document.querySelector(".quiz-box");
+const resultBox = document.querySelector(".result-box");
+const option_list = document.querySelector(".option-list");
+const time_line = document.querySelector("header .time-line");
+const timeText = document.querySelector(".timer time-left");
+const timeCount = document.querySelector(".timer timer-sec");
+
+startBtn.onclick = () => {
+    infoBox.classList.add("activeInfo"); // Show the information box
+
+};
+
+exitBtn.onclick = () => {
+  infoBox.classList.remove("activeInfo"); // Hide the information box
+
+};
+
+continueBtn.onclick = () => {
+  infoBox.classList.remove("activeInfo"); // Hide the information box
+  quizBox.classList.add("activeQuiz"); // Show the Quiz box
+  showQuestions(); //Dislpay Questions
+  startTimer(); // Start Timer countdown
+  startTimerLine(); // Start Timer Line
+  queCounter(1); //Count que
+};
+
+//Variable for quiz
+
+let time_Value = 12;
+let queCount = 0;
+let queNumb = 1;
+let user_Score = 0;
+let counter;
+let counter_Line;
+let widthValue;
+
+
+const restartQuiz = resultBox.querySelector(".buttons .restart");
+const quitQuiz = resultBox.querySelector(".buttons .quit");
+
+restartQuiz.onclick = ()=> {
+  quizBox.classList.add("activeQuiz"); //Show quiz box
+  resultBox.classList.remove("activeResult "); //hide result box
+  time_Value = 12;
+  queCount = 0;
+  queNumb = 1;
+  user_Score = 0;
+  widthValue = 0;
+  showQuestions(queCount);
+  queCounter(queNumb);
+  clearInterval(counter);
+  clearInterval(counter_Line);
+  startTimer(time_Value);
+  startTimerLine(widthValue);
+  timeText.textContent = "Time Left"; //Change the text of timeText
+  next-btn.classList.remove("show");
+
+}
+
+quitQuiz.onclick = ()=>{
+  window.location.reload();
+}
